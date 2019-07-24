8cfe18553bc80d5c96b29f6c25a4ab02741a8f26
Megan Edmonds <meganr.edmonds@gmail.com>
2019-07-24 01:01:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def roll_call_dwarves(array)
-  array.each_with_index{|e, i|
-  puts "#{i+1}. #{e}"}
+  array.each_with_index{|element, index|
+  puts "#{index+1}. #{element}"}
 end
 
 def summon_captain_planet (array)
